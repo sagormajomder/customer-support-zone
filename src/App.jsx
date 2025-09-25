69499fdbaa6391108ce848c8c5c6eb250232b9dd
Sagor Majomder <sagormajomder@gmail.com>
2025-09-25 13:03:54 +0600

    Add TicketStates component for displaying ticket status with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import TicketStates from "./components/TicketStates";
 
 function App() {
   return (
-    <div className="min-h-screen space-y-14 text-[#627382]">
+    <div className="min-h-screen space-y-14 bg-[#F5F5F5] text-[#627382]">
       <Header />
+      <Main>
+        <TicketStates />
+      </Main>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+function Main({ children }) {
+  return (
+    <main>
+      <Container>{children}</Container>
+    </main>
+  );
+}
---
+++ src/components/TicketStates.jsx
@@ -0,0 +1,20 @@
+import vector1 from "../assets/vector1.png";
+
+export default function TicketStates() {
+  return (
+    <div className="xs:grid-cols-2 grid grid-cols-1 gap-4">
+      <div className="rounded-lg bg-linear-[125deg] from-[#632EE3] from-5% to-[#9F62F2] to-85% text-center text-white">
+        <div className="space-y-2.5 rounded-lg bg-contain bg-position-[left,right] bg-no-repeat p-10 md:bg-[url(./assets/vector1.png),url(./assets/vector2.png)]">
+          <p className="text-2xl">In-progress</p>
+          <p className="text-6xl">0</p>
+        </div>
+      </div>
+      <div className="rounded-lg bg-linear-[90deg] from-[#54CF68] from-10% to-[#00827A] to-90% text-center text-white">
+        <div className="space-y-2.5 rounded-lg bg-contain bg-position-[left,right] bg-no-repeat p-10 md:bg-[url(./assets/vector1.png),url(./assets/vector2.png)]">
+          <p className="text-2xl">Resolve</p>
+          <p className="text-6xl">0</p>
+        </div>
+      </div>
+    </div>
+  );
+}
