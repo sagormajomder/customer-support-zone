dacd41e64f268d276441fd4bcabb6d45ef228ac6
Sagor Majomder <sagormajomder@gmail.com>
2025-09-25 14:44:58 +0600

    Add functionality of customer tickets states

--- src/components/TicketStates.jsx
+++ src/components/TicketStates.jsx
@@ -1,16 +1,24 @@
 export default function TicketStates({ customerTickets }) {
+  const customerInProgressTickets = customerTickets.filter(
+    (t) => t.status.toLowerCase() === "pending",
+  ).length;
+
+  const customerResolvedTickets = customerTickets.filter(
+    (t) => t.status.toLowerCase() === "resolved",
+  ).length;
+  console.log(customerTickets);
   return (
     <section className="xs:grid-cols-2 mb-14 grid grid-cols-1 gap-4">
       <div className="rounded-lg bg-linear-[125deg] from-[#632EE3] from-5% to-[#9F62F2] to-85% text-center text-white">
         <div className="space-y-2.5 rounded-lg bg-contain bg-position-[left,right] bg-no-repeat p-10 md:bg-[url(./assets/vector1.png),url(./assets/vector2.png)]">
           <p className="text-2xl">In-progress</p>
-          <p className="text-6xl">0</p>
+          <p className="text-6xl">{customerInProgressTickets}</p>
         </div>
       </div>
       <div className="rounded-lg bg-linear-[90deg] from-[#54CF68] from-10% to-[#00827A] to-90% text-center text-white">
         <div className="space-y-2.5 rounded-lg bg-contain bg-position-[left,right] bg-no-repeat p-10 md:bg-[url(./assets/vector1.png),url(./assets/vector2.png)]">
           <p className="text-2xl">Resolve</p>
-          <p className="text-6xl">0</p>
+          <p className="text-6xl">{customerResolvedTickets}</p>
         </div>
       </div>
     </section>
